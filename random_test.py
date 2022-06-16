236d05444ee6e6ec83ded6bf623659e9a9c215ed
Stephan Skanta <78694630+techcree@users.noreply.github.com>
2022-06-16 08:47:38 +0200

    random
    Generate 4 Numbers between 20 and 100 and print all numbers which add up to 300

---
+++ random_test.py
@@ -0,0 +1,16 @@
+from random import *
+import sys
+
+
+
+while True:
+    a = randint(20, 100) #Range
+    b = randint(20, 100) #Range
+    c = randint(20, 100) #Range
+    d = randint(20, 100) #Range
+    
+    result = (a)+(b)+(c)+(d)
+    
+    if result == 300:
+        print ((a), (b), (c), (d))
+             
